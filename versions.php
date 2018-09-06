0f382e7157cc7e51c26dd07ec0754a61ccd58aad
Nicolai Mihaic <tonci.n@gmail.com>
2018-09-06 17:00:51 -0500

    This the first version of the file

---
+++ versions.php
@@ -0,0 +1,16 @@
+//This is to test the version control on GIT 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Version Control</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <p>hello world this is version number UNO</p>
+</body>
+</html>
